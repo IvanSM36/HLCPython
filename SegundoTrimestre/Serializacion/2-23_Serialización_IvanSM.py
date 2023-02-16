4bddce39904713c9736af49834d1ecba48c7fc40
IvanSM <ivan_sm@hotmail.es>
2023-02-16 10:26:11 +0100

    Initial commit

---
+++ SegundoTrimestre/Serializacion/2-23_Serialización_IvanSM.py
@@ -0,0 +1,8 @@
+# Creo la clase Persona
+class Persona():
+     # Constructor     
+    def __init__(self, nombre, apellidos, edad):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.edad = edad 
+        
